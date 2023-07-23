9a6f95772d7bc5f14676520bd9100515fa59d117
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-07-23 18:58:14 +0900

    [Silver I] Title: 직사각형, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		for(int i = 0; i < 4; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int p1 = Integer.parseInt(st.nextToken());
+			int q1 = Integer.parseInt(st.nextToken());
+			
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int p2 = Integer.parseInt(st.nextToken());
+			int q2 = Integer.parseInt(st.nextToken());
+			
+			char answer = 'a';
+			if(p1 < x2 || q1 < y2 || p2 < x1 || q2 < y1)answer = 'd';
+			else if((p1 == x2 && q1 == y2) || (p1 == x2 && y1 == q2) || (x1 == p2 && q1 == y2)|| (x1 == p2 && y1 == q2))answer = 'c';
+			else if(p1 == x2 || q1 == y2 || x1 == p2 || y1 == q2)answer = 'b';
+			else answer = 'a';
+			
+			System.out.println(answer);
+		}
+	}
+
+}
