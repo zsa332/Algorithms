9699a6dc5cd90b695aaae94a7b6be86d5745753e
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-08-11 10:08:37 +0900

    [Gold V] Title: 암호 만들기, Time: 180 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -4,9 +4,10 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int L, C;
-    static char[] chars;
-    public static void main(String[] args) throws IOException {
+	static int L, C;
+	static char[] chars;
+
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -17,54 +18,64 @@ public class Main {
         chars = str.toCharArray();
 
         Arrays.sort(chars);
-
-        combination(0, 0, 0, "");
-    }
-
-    private static void combination(int start , int cnt, int flag, String password){
-        if(cnt == L){
-            int vowel = 0, consonant = 0;
-            for(int i = 0; i < password.length(); i++){
-                if(password.charAt(i) =='a' || password.charAt(i) =='e' || password.charAt(i) =='i'
-                        || password.charAt(i) =='o' || password.charAt(i) =='u')vowel++;
-                else consonant++;
-            }
-            if(vowel >= 1 && consonant >= 2)
-                System.out.println(password);
-            return;
+        
+        int[] mask = new int[C];
+        for(int i = C - L; i < C; i++) {
+        	mask[i] = 1;
         }
-
-        for(int i = start; i < C; i++){
-            if((flag & 1 << i) != 0) continue;
-            combination(i+1, cnt+1, flag | 1 << i, password + chars[i]);
+        List<String> list = new ArrayList<String>();
+        do{
+        	String line = "";
+        	int v = 0, c = 0;
+        	for(int i = 0; i < C; i++) {
+        		if(mask[i] == 1) {
+        			line += chars[i];
+        			if(chars[i] == 'a' || chars[i] == 'e'||chars[i] == 'i' || chars[i] == 'o' || chars[i] == 'u')
+        				v++;
+        			else c++;
+        		}
+        	}
+        	if(v >= 1 && c >= 2)
+        		list.add(line);
+        }while(np(mask));
+        
+        Collections.sort(list);
+        
+        for(String s : list) {
+        	System.out.println(s);
         }
     }
-    private static boolean np(int[] mask) {
-        // 1. 꼭지점을 탐색
-        int N = mask.length;
-        int i = N - 1;
-        while(i > 0 && mask[i-1] >= mask[i])i--;
 
-        if(i == 0)return false; // 꼭지점이 첫번째인 경우 더이상의 경우의 수 없음
+	private static boolean np(int[] p) {
+		// 1. 꼭지점을 탐색
+		int N = p.length;
+		int i = N - 1;
+		while (i > 0 && p[i - 1] >= p[i])
+			i--;
 
-        // 2. 꼭지점전 값과 바꿀 한단계 높은 값을 탐색
-        int j = N - 1;
-        while(mask[i-1] >= mask[j])j--;
+		if (i == 0)
+			return false; // 꼭지점이 첫번째인 경우 더이상의 경우의 수 없음
 
-        // 3. 교체
-        swap(mask, i-1, j);
+		// 2. 꼭지점전 값과 바꿀 한단계 높은 값을 탐색
+		int j = N - 1;
+		while (p[i - 1] >= p[j])
+			j--;
 
-        // 4. 꼭지점 이후의 값을 오름차순 정렬
-        while(i < j){
-            swap(mask, i++, j--);
-        }
+		// 3. 교체
+		swap(p, i - 1, j);
 
-        return true;
-    }
+		// 4. 꼭지점 이후의 값을 오름차순 정렬
+		int k = N - 1;
+		while (i < k) {
+			swap(p, i++, k--);
+		}
 
-    private static void swap(int[] mask, int i, int j) {
-        int temp = mask[i];
-        mask[i] = mask[j];
-        mask[j] = temp;
-    }
+		return true;
+	}
+
+	private static void swap(int[] mask, int i, int j) {
+		int temp = mask[i];
+		mask[i] = mask[j];
+		mask[j] = temp;
+	}
 }
