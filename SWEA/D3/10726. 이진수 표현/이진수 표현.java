8e6a14facbb4c695c9026d647e1ba0b0aba567c1
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-08-12 14:47:12 +0900

    [D3] Title: 이진수 표현, Time: 143 ms, Memory: 29,772 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,11 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for(int tc = 1; tc <= T; tc++){
@@ -15,10 +15,15 @@ public class Solution {
 
             int checked = (1 << N) - 1;
 
-            String str = "";
-            if((checked & M) == checked)str = "ON";
-            else str = "OFF";
-            System.out.println("#" + tc + " " + str);
+            sb.append("#").append(tc).append(" ");
+            if((checked & M) == checked)sb.append("ON");
+            else sb.append("OFF");
+            sb.append("\n");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
