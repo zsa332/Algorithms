022654acd25a0d25b1979683f26aaee06b1b635d
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-08-18 19:38:55 +0900

    [Gold IV] Title: 샘터, Time: 548 ms, Memory: 233040 KB -BaekjoonHub

--- 백준/Gold/18513. 샘터/샘터.java
+++ 백준/Gold/18513. 샘터/샘터.java
@@ -6,21 +6,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] visited = new boolean[400000001];
+    static boolean[] visited = new boolean[200200001]; // -100,000,000 <= 위치 <= 100,000,000 앞뒤로
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 샘터
+        int K = Integer.parseInt(st.nextToken()); // 집
 
-        int zeroP = visited.length / 2;
+        int zeroP = visited.length / 2; // 0 좌표
         Queue<Structure> q = new ArrayDeque<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int x = zeroP + Integer.parseInt(st.nextToken());
+            int x = zeroP + Integer.parseInt(st.nextToken()); // 샘터 좌표
             visited[x] = true;
-            q.offer(new Structure(x, 0));
+            q.offer(new Structure(x, 0)); // 샘터 입력
         }
 
         int hNum = 0;
@@ -33,7 +33,7 @@ public class Main {
                 if(hNum == K)break;
             }
             int nx = s.x + 1;
-            if(nx <= 400000000 && !visited[nx]){
+            if(nx <= 200200000 && !visited[nx]){
                 visited[nx] = true;
                 q.offer(new Structure(nx, s.dis + 1));
             }
