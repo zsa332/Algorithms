5ea7d73104b650da2a1cf0a081b163fed92907aa
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-09-12 14:11:07 +0900

    [Gold III] Title: 순회강연, Time: 320 ms, Memory: 20228 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -10,34 +10,34 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()); // 강연 요청 수
 		
 		Queue<int[]> queue = new PriorityQueue<>((o1, o2) -> {
-			if(o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
-			return Integer.compare(o2[0], o1[0]);
+			if(o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]); // limit는 오름차순
+			return Integer.compare(o2[0], o1[0]); // pay는 내림차순
 		});
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int pay = Integer.parseInt(st.nextToken());
-			int timeLimit = Integer.parseInt(st.nextToken());
+			int pay = Integer.parseInt(st.nextToken()); // 강연료
+			int timeLimit = Integer.parseInt(st.nextToken()); // 기한
 			
 			queue.add(new int[] {pay, timeLimit});
 		}
 		
-		boolean[] scadule = new boolean[10001];
-		int answer = 0;
+		boolean[] schedule = new boolean[10001]; // 스케쥴 체크
+		int answer = 0; // 최대로 벌 수 있는 금액
 		while(!queue.isEmpty()) {
 			int[] cur = queue.poll();
 			
 			int time = cur[1];
-			while(time > 0) {
-				if(!scadule[time]) {
-					scadule[time] = true;
+			while(time > 0) { // 각 강연은 최대한 마지막 기한에 할 수 있도록 함
+				if(!schedule[time]) {
+					schedule[time] = true;
 					break;
 				}
 				else time--;
 			}
-			if(time > 0)answer += cur[0];
+			if(time > 0)answer += cur[0]; // 강연을 할 수 있다면 합
 		}
 		System.out.println(answer);
 	}
