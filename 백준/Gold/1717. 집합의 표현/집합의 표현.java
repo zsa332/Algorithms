de583299ef4efa1e2033d473708daba948f8c241
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-09-18 16:24:37 +0900

    [Gold V] Title: 집합의 표현, Time: 1232 ms, Memory: 52932 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,50 +4,42 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] arr;
-	public static void main(String[] args) throws IOException{
+	static int[] parents;
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
-		arr = new int[n+1];
-		for(int i = 0; i <= n; i++) {
-			arr[i] = i;
+		parents = new int[N+1];
+		for(int i = 1; i <= N; i++) {
+			parents[i] = i;
 		}
-		
-		for(int i = 0; i < m; i++) {
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int action = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			if(action == 0) {
-				Union(a,b);
-			}
-			else if(action == 1) {
-				int rootA = Find(a);
-				int rootB = Find(b);
-				if(rootA == rootB)System.out.println("YES");
+			if(c == 0) union(a, b);
+			if(c == 1) {
+				if(find(a) == find(b))System.out.println("YES");
 				else System.out.println("NO");
 			}
 		}
 	}
-
-	private static int Find(int target) {
-		if(target == arr[target])return target;
-		else {
-			arr[target] = Find(arr[target]);
-			return arr[target];
-		}
+	
+	public static void union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if(aRoot == bRoot)return;
+		
+		parents[bRoot] = parents[aRoot]; 
 	}
 
-	private static void Union(int a, int b) {
-		int rootA = Find(a);
-		int rootB = Find(b);
-		int root = Math.min(rootA, rootB);
-		arr[rootA] = root;
-		arr[rootB] = root;
+	private static int find(int a) {
+		if(a == parents[a])return a;
+		return parents[a] = find(parents[a]);
 	}
 }
