3eee335f63589ede67e2640b4d2e414c25c5c1bb
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2023-10-22 22:37:16 +0900

    [Silver III] Title: 게임, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(st.nextToken());
+        int Y = Integer.parseInt(st.nextToken());
+        int Z = getPercent(X, Y);
+
+        int ans = -1;
+        int left = 0;
+        int right = (int) 1e9;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (getPercent(X + mid, Y + mid) != Z) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static int getPercent(int x, int y) {
+        return (int) ((long) y * 100 / x);
+    }
+}
