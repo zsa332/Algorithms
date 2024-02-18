c0fdfc2692e8c98f1781a34f4dfa1b8cda7d5e2c
SeongWon Seo <78728865+zsa332@users.noreply.github.com>
2024-02-18 16:44:40 +0900

    [Gold V] Title: 개똥벌레, Time: 324 ms, Memory: 31008 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, H;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        int[] bot = new int[H + 2];
+        int[] top = new int[H + 2];
+
+        for (int i = 0; i < N / 2; i++) {
+            bot[Integer.parseInt(br.readLine())]++;
+            top[H - Integer.parseInt(br.readLine()) + 1]++;
+        }
+
+        for (int i = 1; i <= H; i++) {
+            bot[i] += bot[i - 1];
+        }
+
+        for (int i = H; i >= 1; i--) {
+            top[i] += top[i + 1];
+        }
+
+        int min = N;
+        int cnt = 0;
+
+        for (int i = 1; i <= H; i++) {
+            int obs = (bot[H] - bot[i - 1]) + (top[1] - top[i + 1]);
+
+            if (obs < min) {
+                min = obs;
+                cnt = 1;
+            } else if (obs == min)
+                cnt++;
+        }
+
+        System.out.println(min + " " + cnt);
+    }
+}
